875f9b67ed5e5c044e62f2ef3ac456a86bdf2f24
Abbas Sarwar <abbassarwar100@gmail.com>
2023-07-31 21:26:55 +0500

    Changes Models created Recipes User food food_recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :foods
+  has_many :recipes
+  has_many :through_recipes, through: :recipes, source: :recipe_foods
+  has_many :through_food, through: :foods, source: :recipe_foods
+
+  validates :name, presence: true
 end
