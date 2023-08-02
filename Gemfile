bb7138671485bb02bfd5ead94d92f03687fe9694
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-02 10:44:17 +0530

    fixed linters

--- Gemfile
+++ Gemfile
@@ -5,9 +5,9 @@ ruby '3.1.3'
 gem 'cancancan'
 gem 'devise'
 
-gem 'pg'
 gem 'bootstrap', '~> 5.0'
 gem 'bootstrap_form', '~> 5.2'
+gem 'pg'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.6'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :update_allowed_parameters, if: :devise_controller?
-   
-    protected
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:email, :password)
-      end
-end
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :password)
+    end
+  end
 end
