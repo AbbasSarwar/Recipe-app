d2cc8b48e58e06b1c21eb5a4f89614b519752685
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-03 08:41:26 +0530

    updated recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
 
-  has_many :recipe_foods,  dependent: :destroy
-  has_many :foods, through: :recipe_foods
+  has_many :ingredients,  dependent: :destroy
+  has_many :foods, through: :ingredients
 
 
   validates :name, presence: true, uniqueness: {scope: :user_id}
