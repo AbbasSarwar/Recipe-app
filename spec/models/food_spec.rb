73b12cec8912440f307fcb364ee688cb6bbc2e9e
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-04 14:43:27 +0530

    fixed linters

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,22 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Food, type: :model do
   describe 'validations' do
-
     let(:food) { Food.new }
-     before do 
-      food.valid? 
-     end
-
-    it "requires a user" do
-        expect(food.errors[:user]).to include("can't be blank")
+    before do
+      food.valid?
     end
 
-    it "requires a name" do
+    it 'requires a user' do
+      expect(food.errors[:user]).to include("can't be blank")
     end
-
-   end
-
+  end
 end
-
-
-  
