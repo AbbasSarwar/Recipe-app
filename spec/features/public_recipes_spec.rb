d0b08de4a58cc7f6eddbc0d4f3176b35f6bc91f3
Abbas Sarwar <abbassarwar100@gmail.com>
2023-08-05 18:48:58 +0500

    errors

--- spec/features/public_recipes_spec.rb
+++ spec/features/public_recipes_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
-RSpec.describe 'Recipes index page', type: :feature do
-  let(:user) { create(:user) }
-  let!(:recipe) { create(:recipe, user:) }
 
+RSpec.describe 'Public recipes page', type: :feature do
   before do
-    login_as(user)
+    @user = User.create(name: "abbas", email: "abbas@gmail.com", password: "abbas.123")
+    @recipe = Recipe.create(name: "qorma", preparation_time: 120, cooking_time: 60, description: "recipe", public: true)
+    login_as(@user)
     visit recipes_path
   end
 
   it "Enable public should be able to Display in Public" do
-    click_link recipe.name
-    first('.btn').click
+    click_link "qorma"
     visit public_recipes_path
-    expect(page).to have_content(recipe.name)
+    expect(page).to have_content(@recipe.name)
   end
 end
---
+++ spec/features/shopping_list_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe 'Recipe show page', type: :feature do
+  let(:user) { create(:user) }
+  let(:recipe) { create(:recipe, user:) }
+
+  before do
+    login_as(user)
+    visit recipe_path(recipe)
+  end
+  it "shopping list display" do
+  end
+end
