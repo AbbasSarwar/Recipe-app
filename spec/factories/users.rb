54498ac3f0adad71de45c9adbbf697772e91ad75
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-05 20:20:08 +0530

    updated public_recipes_spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    sequence(:name) { |n| "User #{n}" } # To generate unique names
     email { Faker::Internet.unique.email }
     password { 'password123' }
     password_confirmation { 'password123' }
--- spec/features/public_recipes_spec.rb
+++ spec/features/public_recipes_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
-RSpec.describe 'Recipes index page', type: :feature do
-  let(:user) { create(:user) }
-  let!(:recipe) { create(:recipe, user:) }
 
-  before do
-    login_as(user)
+RSpec.describe 'Recipes index page', type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  around do |testitem|
+    food = FactoryBot.create(:food, name: 'Apple', price: 10.0)
+    @recipe = FactoryBot.create(:recipe, name: 'Apple Pie', user: user)
+    ingredient = FactoryBot.create(:ingredient, recipe: @recipe, food: food, quantity: 3)
+    sign_in(user)
     visit recipes_path
+    testitem.run
+    user.recipes.destroy_all
+    user.destroy # Clean up the user after the test
   end
 
-  it "Enable public should be able to Display in Public" do
-    click_link recipe.name
-    first('.btn').click
-    visit public_recipes_path
-    expect(page).to have_content(recipe.name)
+  it "displays public recipes with their details" do
+    within('.card') do
+      expect(page).to have_content(@recipe.name)
+      #expect(page).to have_content("By:- #{user.name}")
+      #expect(page).to have_content("Food items# #{@recipe.ingredients.count}") # Since we created 1 ingredient for the recipe
+      expect(page).to have_content("#{@recipe.ingredients.sum {|ingredient| ingredient.food.price}}") # 3 * $10 (quantity * food price)
+    end
   end
 end
